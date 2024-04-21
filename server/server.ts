a0e77cee39e91773ae449e196db405e6b953c4a8
Tobias0013 <tobias.vinblad@hotmail.com>
2024-04-21 18:47:55 +0200

    create v1 of template

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { port } from "./controller/config"
+import router from "./routes";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.use("/", router);
+
+app.listen(port, () => {
+    console.log(`Server started successfully.\nListening on http://localhost:${port}`);
+});
